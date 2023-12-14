437f9365c181ad35c707fc1a8c472798c6aa0abe
braydencw1 <50538489+braydencw1@users.noreply.github.com>
2023-12-13 23:56:14 -0600

    Challenge 1

--- maelstrom-echo/go.mod
+++ maelstrom-echo/go.mod
@@ -2,4 +2,7 @@ module maelstrom-echo
 
 go 1.20
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231205140322-b59de21565d8 // indirect
+require (
+	github.com/google/uuid v1.5.0
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231205140322-b59de21565d8
+)
--- maelstrom-echo/main.go
+++ maelstrom-echo/main.go
@@ -4,17 +4,39 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+// maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+// challenge 0
+//
+//	func main() {
+//		n := maelstrom.NewNode()
+//		n.Handle("echo", func(msg maelstrom.Message) error {
+//			var body map[string]any
+//			if err := json.Unmarshal(msg.Body, &body); err != nil {
+//				return nil
+//			}
+//			body["type"] = "echo_ok"
+//
+//			return n.Reply(msg, body)
+//		})
+//		if err := n.Run(); err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+
+// challenge 1
 func main() {
 	n := maelstrom.NewNode()
-	n.Handle("echo", func(msg maelstrom.Message) error {
+	n.Handle("generate", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return nil
 		}
-		body["type"] = "echo_ok"
+		body["type"] = "generate_ok"
+		body["id"] = uuid.New()
 
 		return n.Reply(msg, body)
 	})
